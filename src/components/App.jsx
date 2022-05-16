1bd0ad7541941d3e12b684b6fa10087b07b5cf9b
tsuccar <tsuccar@users.noreply.github.com>
2022-05-15 22:32:51 -0400

    Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  setInterval(updateTime, 1000);
+
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1 style={{ whiteSpace: "nowrap" }}>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
+
+//three ways to target the text wrapping
+// <h1 style={{whiteSpace: "nowrap"}}>{time}</h1>
+// const now = new Date().toLocaleTimeString()..substring(0,8)
+// const now = new Date().toLocaleTimeString([], { hour12: false })
 
 export default App;
